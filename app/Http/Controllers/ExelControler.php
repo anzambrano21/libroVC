69dd613f9edabd67bc04f093cad2cd91ba9c6485
anzambrano21 <lolepico1234@gmail.com>
2024-08-04 15:57:56 -0400

    pruevas

--- app/Http/Controllers/ExelControler.php
+++ app/Http/Controllers/ExelControler.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Exports\UseExport;
+use Maatwebsite\Excel\Facades\Excel;
 class ExelControler extends Controller
 {
     public function Exel() {
-        
+        return Excel::download(new UseExport,"prueva.xlsx" );
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExelControler;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/export',[ExelControler::class,"Exel"]);
 Route::get('/{any?}', function () {
     return view('welcome');
 });
